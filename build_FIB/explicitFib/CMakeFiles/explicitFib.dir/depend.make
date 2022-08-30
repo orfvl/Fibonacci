93df3ebe3e538e6322c655fd2f9a0cdf3498040a
orfvl <oscarvonloehneysen@gmail.com>
2022-08-30 19:56:44 +0200

    removed build files that i left in cause i am a bob

--- build_FIB/explicitFib/CMakeFiles/explicitFib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for explicitFib.
-# This may be replaced when dependencies are built.
--- build_FIB/simpleFIB/CMakeFiles/simpleFIB.dir/simpleFIB.cpp.o.d
+++
@@ -1,4 +0,0 @@
-simpleFIB/CMakeFiles/simpleFIB.dir/simpleFIB.cpp.o: \
- /home/oscar/Documents/Informatik/Fibonacci/simpleFIB/simpleFIB.cpp \
- /usr/include/stdc-predef.h \
- /home/oscar/Documents/Informatik/Fibonacci/simpleFIB/simpleFIB.h
